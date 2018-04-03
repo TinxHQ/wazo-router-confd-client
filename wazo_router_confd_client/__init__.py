8e3a731803622fca2b1f83ccb0f5338673612cc5
Sylvain Boily <sylvain@wazo.io>
2018-04-03 01:24:57 +0000

    fix copy/paste

--- wazo_router_confd_client/__init__.py
+++ wazo_router_confd_client/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from nestbox_confd_client.client import ConfdClient as Client
+from wazo_router_confd_client.client import ConfdClient as Client
 
 __all__ = [
     'Client',
